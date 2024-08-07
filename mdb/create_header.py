2aa530e36f83cf67d2af830b90aaccfa3c7e874e
Yuka Furuta <y.furuta@ist.hokudai.ac.jp>
2024-08-07 18:39:01 +0900

    create_header yaml version

--- mdb/create_header.py
+++ mdb/create_header.py
@@ -1,9 +1,7 @@
+from ruamel.yaml import YAML
 from enum import Enum
-
 from yamcs.pymdb import *
-
 import sys
-
 
 
 class Subsystem(Enum):
@@ -14,47 +12,49 @@ class Subsystem(Enum):
     SRS3 = 21
 
 
-system = System("SCSAT1")
-csp_header_name="csp-header"
+def main():
+    yaml = YAML()
+    yaml_file = "/home/yuka/myproject/task/scsat1-mcs/mdb/data/header.yaml"
 
-csp_list=[
-    ["csp-priority", uint2_t, 0],
-    ["csp-source-id", uint5_t, 0],
-    ["csp-dst-id", uint5_t, 0],
-    ["csp-dst-port", uint6_t, 0],
-    ["csp-source-port", uint6_t, 0],
-    ["csp-reserved", uint4_t, 0],
-    ["csp-hmac", uint1_t, 0],
-    ["csp-xtea", uint1_t, 0],
-    ["csp-rdp", uint1_t, 0],
-    ["csp-crc", uint1_t, 0],
-]
+    with open(yaml_file, 'r') as file:
+        data = yaml.load(file)
+
+    system = System("SCSAT1")
+    csp_header_name="csp-header"
 
 
-general_container = Container(
-    system=system,
-    name="scsat1",
-    abstract=True,
-)
-
-
-entries_list=[]
-for l in csp_list:
-    tm = IntegerParameter(
+    general_container = Container(
         system=system,
-        name=l[0],
-        signed=False,
-        encoding=l[1],
-    )
-    entries_list.append(ParameterEntry(tm))
-
-
-container = Container(
-        system=system,
-        name=csp_header_name,
-        base=general_container ,
-        entries=entries_list
+        name="scsat1",
+        abstract=True,
     )
 
-with open("mdb/scsat1_header.xml", "wt") as f:
-    system.dump(f)
+
+    entries_list=[]
+
+    for key in data["parameters"].keys():
+        for param in data["parameters"][key]:
+            tm = IntegerParameter(
+                system=system,
+                name=param["name"],
+                signed=False,
+                encoding=globals()[param["type"]]
+            )
+            entries_list.append(ParameterEntry(tm))
+
+
+    container = Container(
+            system=system,
+            name=csp_header_name,
+            base=general_container ,
+            entries=entries_list
+        )
+
+    with open("mdb/scsat1_header.xml", "wt") as f:
+        system.dump(f)
+
+
+
+
+if __name__ == '__main__':
+    main()
