8bb73766aac6536990f485701437998ed94bdc21
Yuka-Furuta <y.furuta@ist.hokudai.ac.jp>
2025-01-21 11:52:32 +0900

    Add Docker support for running scsat1-mcs and grafana
    Running `docker-compose up -d` in the docker directory
    
    Signed-off-by: Yuka Furuta <y.furuta@ist.hokudai.ac.jp>

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#/bin/sh -e
+dpkg -i yamcs_5.9.3-1_amd64.deb
+set -e
+
+mkdir -p ~/yamcs_work
+cd ~/yamcs_work || exit
+git clone https://github.com/spacecubics/scsat1-mcs.git
+
+cd ./scsat1-mcs || exit
+pip install -r requirements.txt
+./scripts/inst_cfg_mdb.sh ~/yamcs_work
+chmod a+rw /opt/yamcs/cache/
+chmod a+rw /opt/yamcs/log/
+
+cd ..
+old_address="localhost"
+new_address="0.0.0.0"
+sed -i "s/${old_address}/${new_address}/g" ./etc/yamcs.yaml
+/opt/yamcs/bin/yamcsd --etc-dir ~/yamcs_work/etc/ 
