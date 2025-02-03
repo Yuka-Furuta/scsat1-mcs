24c6f08b71705c254df585861245a990579c83b5
Yuka-Furuta <y.furuta@ist.hokudai.ac.jp>
2025-02-03 21:21:01 +0900

    Enable grafana admin initial setting modification
    - Implemented functionality to change the admin username and password.
    - Updated README and JSON file name for better clarity.
    
    Signed-off-by: Yuka Furuta y.furuta@ist.hokudai.ac.jp

--- docker/grafana/import.sh
+++ docker/grafana/import.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-FILE_NAME=$1
-sed -i 's/\\/\\\\/g' my_dashboard.json
+file_name="scsat1_ini.json"
+sed -i 's/\\/\\\\/g' ${file_name}
+if [ -z "$GF_SECURITY_ADMIN_USER" ]; then
+    GF_SECURITY_ADMIN_USER="admin"
+fi
+if [ -z "$GF_SECURITY_ADMIN_PASSWORD" ]; then
+    GF_SECURITY_ADMIN_PASSWORD="admin"
+fi
+password=`echo -n "${GF_SECURITY_ADMIN_USER}:${GF_SECURITY_ADMIN_PASSWORD}" | base64`
+
 while read LINE
 do
     JSON_DATA=`echo ${LINE} | jq -r .dashboard.id=null`
     result_m=`curl -s -X POST \
     -H "Accept: application/json" \
     -H "Content-Type: application/json" \
-    -H "Authorization: Basic YWRtaW46YWRtaW4=" \
+    -H "Authorization: Basic ${password}" \
     -H "${GRAFANA_API_KEY}" \
     -d "${JSON_DATA}" \
     http://admin:admin@${MY_IP}:3000/api/dashboards/db`
@@ -17,7 +25,7 @@ do
         curl -s -X POST \
         -H "Accept: application/json" \
         -H "Content-Type: application/json" \
-        -H "Authorization: Basic YWRtaW46YWRtaW4=" \
+        -H "Authorization: Basic ${password}" \
         -H "${GRAFANA_API_KEY}" \
         -d "${folder_data}" \
         http://admin:admin@${MY_IP}:3000/api/folders
@@ -25,9 +33,9 @@ do
         curl -s -X POST \
         -H "Accept: application/json" \
         -H "Content-Type: application/json" \
-        -H "Authorization: Basic YWRtaW46YWRtaW4=" \
+        -H "Authorization: Basic ${password}" \
         -H "${GRAFANA_API_KEY}" \
         -d "${JSON_DATA}" \
         http://admin:admin@${MY_IP}:3000/api/dashboards/db
     fi
-done < ${FILE_NAME}
+done < ${file_name}
--- docker/grafana/setup.sh
+++ docker/grafana/setup.sh
@@ -1,30 +1,40 @@
 #!/bin/bash -e
+if [ -z "$GF_SECURITY_ADMIN_USER" ]; then
+    GF_SECURITY_ADMIN_USER="admin"
+fi
+if [ -z "$GF_SECURITY_ADMIN_PASSWORD" ]; then
+    GF_SECURITY_ADMIN_PASSWORD="admin"
+fi
+password=`echo -n "${GF_SECURITY_ADMIN_USER}:${GF_SECURITY_ADMIN_PASSWORD}" | base64`
+
 GRAFANA_SERVICE_ID=`curl -s -X POST \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
--H "Authorization: Basic YWRtaW46YWRtaW4=" \
+-H "Authorization: Basic ${password}" \
 -d '{"name":"test", "role": "Editor"}' \
 http://admin:admin@${MY_IP}:3000/api/serviceaccounts | jq -r .id`
 
+# Export service account to enable continued API execution in the same shell
 export GRAFANA_SERVICE_ID
+# Save the service account to grafana_profile for reuse when re-entering the container
 echo "export GRAFANA_SERVICE_ID=${GRAFANA_SERVICE_ID}" >> ~/.grafana_profile 
 
 GRAFANA_API_KEY=`curl -s -X POST \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
--H "Authorization: Basic YWRtaW46YWRtaW4=" \
+-H "Authorization: Basic ${password}" \
 -d '{"name":"test"}' \
 http://admin:admin@${MY_IP}:3000/api/serviceaccounts/${GRAFANA_SERVICE_ID}/tokens | jq -r .key`
 
+# Export token to enable continued API execution in the same shell
 export GRAFANA_API_KEY
+# Save the token to grafana_profile for reuse when re-entering the container
 echo "export GRAFANA_API_KEY=${GRAFANA_API_KEY}" >> ~/.grafana_profile 
 
 curl -s -X POST \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
--H "Authorization: Basic YWRtaW46YWRtaW4=" \
+-H "Authorization: Basic ${password}" \
 -H "${GRAFANA_API_KEY}" \
 -d '{"name":"yamcs-ds1", "type":"yamcs-yamcs-datasource", "url":"http://yamcs:8090", "access":"proxy", "basicAuth":false, "jsonData":{"instance":"scsat1"}}' \
 http://admin:admin@${MY_IP}:3000/api/datasources
-
-
